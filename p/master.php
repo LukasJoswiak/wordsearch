8b61951e5104aba9d4c2e7bb718ff27e02ceafb5
Lukas <admin@lukasjoswiak.com>
2014-07-27 12:56:10 -0700

    added twitter tweet button

--- p/master.php
+++ p/master.php
@@ -33,11 +33,18 @@
 
 	  ga('create', 'UA-10943451-27', 'lukasjoswiak.com');
 	  ga('send', 'pageview');
+	</script>
 
+	<script>
+		!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
 	</script>
 
 	<header>
 		<h1><a href="/">Word Search Solver</a></h1>
+
+		<div id="shareButtons">
+			<a href="https://twitter.com/share" class="twitter-share-button" data-lang="en" data-size="large" data-text="Check out this solved word search!">Tweet</a>
+		</div>
 	</header>
 
 	<main>
