a9879681ecda6c96286d95cf68ef308c950cd546
Lukas Joswiak <lukas@lukasjoswiak.com>
2020-06-17 22:45:10 -0700

    Add javascript to add and remove words from form

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,6 +14,57 @@ function autoResize(textarea) {
     textarea.style.height = (textarea.scrollHeight + 2) + "px";
 }
 
+// Override enter to add a new word instead of submit word form.
+var wordForm = document.getElementById("word-form");
+if (wordForm != null) {
+    wordForm.onkeypress = function(e) {
+        var key = e.keyCode || e.which;
+        if (key == 13) {
+            e.preventDefault();
+            addWord();
+            return false;
+        }
+    }
+
+    // Delete input when backspace is pressed and the input is empty.
+    var wordInputs = document.querySelectorAll("#inner-words > input[type='text']");
+    console.log(wordInputs);
+    for (var i = 0; i <  wordInputs.length; i++) {
+        wordInputs[i].onkeydown = function(e) {
+            var key = e.keyCode || e.which;
+            if (key == 8 && this.value.length == 0) {
+                var removeInputs = document.querySelectorAll("input[name='" + this.name + "']");
+                for (var j = 0; j < removeInputs.length; j++) {
+                    removeInputs[j].classList.add("hidden");
+                }
+            }
+        }
+    }
+
+    // Add a new word input when the add word button is clicked.
+    var addWordButton = document.getElementById("add-word");
+    var innerWords = document.getElementById("inner-words");
+    addWordButton.onclick = addWord;
+
+    function addWord() {
+        var words = document.querySelectorAll("#inner-words > input").length / 2;
+        var inputName = "Words." + words + ".Word";
+
+        var hiddenInput = document.createElement("input");
+        hiddenInput.type = "hidden";
+        hiddenInput.name = inputName;
+
+        var input = document.createElement("input");
+        input.type = "text";
+        input.name = hiddenInput.name;
+
+        innerWords.append(hiddenInput);
+        innerWords.append(input);
+
+        input.focus();
+    }
+}
+
 // Add mouseover detection for each word found in the puzzle.
 const selectedClass = "selected";
 var words = document.querySelectorAll(".found");
